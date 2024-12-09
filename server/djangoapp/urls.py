51d9cbeb7716355820af1f292f11fd6beac65c79
Anoop Babu <anoop33@outlook.com>
2024-12-09 17:36:46 -0500

    added car make and car model models in djangoapp

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -7,7 +7,8 @@ app_name = 'djangoapp'
 urlpatterns = [
     path(route='register', view=views.registration, name='register'),
     path(route='login', view=views.login_user, name='login'),
-    path(route="logout", view=views.logout_user, name="logout")
+    path(route="logout", view=views.logout_user, name="logout"),
+    path(route="get_cars", view=views.get_cars, name="getcars"),
     # path for dealer reviews view
 
     # path for add a review view
